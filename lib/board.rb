94d22335af4daf5dc7ac8bb750983e6db20dc3b6
Cashrel894 <cashrel23@sjtu.edu.cn>
2026-01-14 16:07:58 +0800

    feat: implement basic functions of Board class
    Implement management and display functions for Board class

---
+++ lib/board.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+# A 8*8 grid that manages the current game state.
+class Board
+  BOARD_WIDTH = 8
+  PLACEHOLDER_CHAR = ' '
+
+  def initialize
+    @grid = Array.new(BOARD_WIDTH) { Array.new(BOARD_WIDTH, nil) }
+  end
+
+  def piece_at(rank, file)
+    @grid[rank][file]
+  end
+
+  def add(rank, file, piece)
+    @grid[rank][file] = piece
+  end
+
+  def remove(rank, file)
+    @grid[rank][file] = nil
+  end
+
+  def to_s
+    str = ''
+    @grid.each do |row|
+      row.each do |piece|
+        str += piece.nil? ? PLACEHOLDER_CHAR : piece
+      end
+      str += "\n"
+    end
+    str
+  end
+end
