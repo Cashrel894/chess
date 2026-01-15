6305d9fa4f78d56a0730408d4bac62d92eb4defc
Cashrel894 <cashrel23@sjtu.edu.cn>
2026-01-15 10:07:12 +0800

    feat: extend the functionality of the base Piece class
    The Piece class now provides friendfire verification & symbol representation & inspection for debugging.

--- lib/piece.rb
+++ lib/piece.rb
@@ -2,6 +2,12 @@
 
 # An object that manages how a piece can move and display.
 class Piece
+  BLACK_SYMBOL = '🦀'
+  WHITE_SYMBOL = '🐌'
+  FALLBACK_SYMBOL = '🐈'
+
+  attr_accessor :player
+
   def initialize(board, rank, file, player)
     @board = board
     @rank = rank
@@ -10,11 +16,44 @@ class Piece
   end
 
   # The core method of Piece class, and it should be implemented by its derived classes.
-  # First, verifies whether the move towards the target square is valid.
+  # First, verifies whether the move towards the target square is legal.
   # If it is, carries out the move by changing the game state.
   # If not, raises a MoveError that should be handled by the caller.
   def move!(tgt_rank, tgt_file)
-    raise NotImplementedError
+    verify_legality(tgt_rank, tgt_file)
+
+    @board.remove(@rank, @file)
+    @board.add(tgt_rank, tgt_file, self)
+  end
+
+  def verify_legality(tgt_rank, tgt_file)
+    verify_path(tgt_rank, tgt_file)
+    verify_capture(tgt_rank, tgt_file)
+  end
+
+  def verify_path(tgt_rank, tgt_file) end
+
+  def verify_capture(tgt_rank, tgt_file)
+    capture = @board.piece_at(tgt_rank, tgt_file)
+
+    return if capture.nil?
+    raise FriendFireError if capture.player == @player
+  end
+
+  # For now, pieces' outlooks are hard-coded at such an early development stage.
+  # Player symbols are alike.
+  def to_s
+    if player == :black
+      BLACK_SYMBOL
+    elsif player == :white
+      WHITE_SYMBOL
+    else
+      FALLBACK_SYMBOL
+    end
+  end
+
+  def inspect
+    "<#{player}'s #{self.class.name} at (#{rank}, #{file})>"
   end
 end
 
