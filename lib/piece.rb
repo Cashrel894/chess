92b92127655e5009b5c1d551f0201d584e93dcc4
Cashrel894 <cashrel23@sjtu.edu.cn>
2026-01-16 21:04:23 +0800

    feat: implement Rook, Bishop and Queen
    Also introduces a helper module to help with further implementation.
    Note that the Piece class is changed a lot:
    1.Piece representation is now implemented through instance methods
    2.Piece's verifying functionality is enhanced and split out as a module

--- lib/piece.rb
+++ lib/piece.rb
@@ -1,14 +1,54 @@
 # frozen_string_literal: true
 
 require_relative 'utils/chess_errors'
+require_relative 'utils/chess_helpers'
+
+# Includes Piece's methods in charge of verifying whether a move is legal.
+module MoveVerifier
+  def verify_legality(tgt_rank, tgt_file)
+    verify_path(tgt_rank, tgt_file)
+    verify_capture(tgt_rank, tgt_file)
+  end
+
+  def verify_path(tgt_rank, tgt_file)
+    verify_reach(tgt_rank, tgt_file)
+    verify_blocked(tgt_rank, tgt_file)
+  end
+
+  def verify_reach(tgt_rank, tgt_file)
+    return true if reachable?(tgt_rank, tgt_file)
+
+    throw OutOfReachError(piece: self, tgt_rank: tgt_rank, tgt_file: tgt_file)
+  end
+
+  def reachable?(tgt_rank, tgt_file)
+    true
+  end
+
+  def verify_blocked(tgt_rank, tgt_file)
+    return true unless blocked?(tgt_rank, tgt_file)
+
+    throw BlockedError(piece: self, tgt_rank: tgt_rank, tgt_file: tgt_file)
+  end
+
+  def blocked?(tgt_rank, tgt_file)
+    false
+  end
+
+  def verify_capture(tgt_rank, tgt_file)
+    capture = @board.piece_at(tgt_rank, tgt_file)
+
+    return if capture.nil?
+    raise FriendFireError.new(piece: self, tgt_rank: tgt_rank, tgt_file: tgt_file) if capture.player == @player
+
+    true
+  end
+end
 
 # An object that manages how a piece can move and display.
 class Piece
+  include MoveVerifier
   include ChessErrors
-
-  BLACK_SYMBOL = '🦀'
-  WHITE_SYMBOL = '🐌'
-  FALLBACK_SYMBOL = '🐈'
 
   attr_accessor :board, :rank, :file, :player
 
@@ -19,6 +59,18 @@ class Piece
     @player = player
 
     board.add(@rank, @file, self)
+  end
+
+  def black_symbol
+    '🦀'
+  end
+
+  def white_symbol
+    '🐌'
+  end
+
+  def fallback_symbol
+    '🐈'
   end
 
   # The core method of Piece class, and it should be implemented by its derived classes.
@@ -36,33 +88,15 @@ class Piece
     true
   end
 
-  def verify_legality(tgt_rank, tgt_file)
-    verify_path(tgt_rank, tgt_file)
-    verify_capture(tgt_rank, tgt_file)
-  end
-
-  def verify_path(tgt_rank, tgt_file)
-    true
-  end
-
-  def verify_capture(tgt_rank, tgt_file)
-    capture = @board.piece_at(tgt_rank, tgt_file)
-
-    return if capture.nil?
-    raise FriendFireError.new(piece: self, tgt_rank: tgt_rank, tgt_file: tgt_file) if capture.player == @player
-
-    true
-  end
-
   # For now, pieces' outlooks are hard-coded at such an early development stage.
   # Player symbols are alike.
   def to_s
     if player == :black
-      BLACK_SYMBOL
+      black_symbol
     elsif player == :white
-      WHITE_SYMBOL
+      white_symbol
     else
-      FALLBACK_SYMBOL
+      fallback_symbol
     end
   end
 
@@ -74,25 +108,64 @@ end
 # The derived classes are as follows
 # TODO: split them into independent files
 
+# Manages a single Queen,
+# which moves any number of squares in any straight or diagonal direction.
+class Queen < Piece
+  include ChessHelpers::MoveHelpers::QueenHelpers
+
+  def black_symbol
+    '♛'
+  end
+
+  def white_symbol
+    '♕'
+  end
+
+  def reachable?(tgt_rank, tgt_file)
+    queen_reachable?(self, tgt_rank, tgt_file)
+  end
+
+  def blocked?(tgt_rank, tgt_file)
+    queen_blocked?(self, tgt_rank, tgt_file)
+  end
+end
+
 # Manages a single Rook,
 # which moves any number of squares horizonally or vertically.
-class Rook < Piece
+class Rook < Queen
+  def black_symbol
+    '♜'
+  end
+
+  def white_symbol
+    '♖'
+  end
+
+  def reachable?(tgt_rank, tgt_file)
+    rook_reachable?(self, tgt_rank, tgt_file)
+  end
 end
 
 # Manages a single Bishop,
 # which moves any number of squares diagonally.
-class Bishop < Piece
+class Bishop < Queen
+  def black_symbol
+    '♝'
+  end
+
+  def white_symbol
+    '♗'
+  end
+
+  def reachable?(tgt_rank, tgt_file)
+    bishop_reachable?(self, tgt_rank, tgt_file)
+  end
 end
 
 # Manages a single Knight,
 # which moves in an "L" shape: two squares in one direction, then one square perpendicular.
 # It can jump over other pieces.
 class Knight < Piece
-end
-
-# Manages a single Queen,
-# which moves any number of squares in any straight or diagonal direction.
-class Queen < Piece
 end
 
 # Manages a single King,
---
+++ lib/utils/chess_helpers.rb
@@ -0,0 +1,125 @@
+# frozen_string_literal: true
+
+# Includes a bunch of helper methods for the chess game.
+module ChessHelpers
+  # Includes helper methods for Pieces' move! methods implementation.
+  module MoveHelpers
+    # Includes helper methods for pieces that moves any number of squares in any straight or diagonal directions.
+    # Despite its name, it's also for Rook and Bishop, since Queen is just a combination of the two in some way.
+    module QueenHelpers
+      # A helper class tailored for piece movement on chess board.
+      # It only supports 8 directions, 4 for straight and 4 for digonal.
+      class Vector
+        attr_accessor :rank, :file
+
+        def initialize(end_rank, end_file, begin_rank = 0, begin_file = 0)
+          @rank = end_rank - begin_rank
+          @file = end_file - begin_file
+        end
+
+        # Transforms the vector to its corresponding unit vector.
+        # Only works with staright or diagonal directions. If not, it throws an error.
+        # Note that when working with diagonal directions, it gets a "unit vector" like (1, 1),
+        # instead of something like (1/sqrt(2), 1/sqrt(2)), since it makes no sense in this context.
+        # Specially, when working with zero vectors, it does nothing.
+        def unitize
+          e = StandardError.new('Exception in vector unitization: not a straight or diagonal direction.')
+          throw e unless straight_or_diagonal?
+
+          unit_vec = clone
+
+          unit_vec.rank /= unit_vec.rank.abs unless unit_vec.rank.zero?
+          unit_vec.file /= unit_vec.file.abs unless unit_vec.file.zero?
+          unit_vec
+        end
+
+        def len
+          [@rank.abs, @file.abs].max
+        end
+
+        def +(other)
+          Vector.new(@rank + other.rank, @file + other.file)
+        end
+
+        def *(other)
+          Vector.new(@rank * other, @file * other)
+        end
+
+        def straight?
+          @rank.zero? || @file.zero?
+        end
+
+        def diagonal?
+          @rank.abs == @file.abs
+        end
+
+        def straight_or_diagonal?
+          straight? || diagonal?
+        end
+
+        def zero?
+          @rank.zero? && @file.zero?
+        end
+
+        def inspect
+          "(#{@rank}, #{@file})"
+        end
+
+        def to_s
+          inspect
+        end
+      end
+
+      def pos_of(piece)
+        Vector.new(piece.rank, piece.file)
+      end
+
+      def direction_of(piece, tgt_rank, tgt_file)
+        Vector.new(tgt_rank, tgt_file, piece.rank, piece.file)
+      end
+
+      def rook_reachable?(rook, tgt_rank, tgt_file)
+        direction_of(rook, tgt_rank, tgt_file).straight?
+      end
+
+      def bishop_reachable?(bishop, tgt_rank, tgt_file)
+        direction_of(bishop, tgt_rank, tgt_file).diagonal?
+      end
+
+      def queen_reachable?(queen, tgt_rank, tgt_file)
+        rook_reachable?(queen, tgt_rank, tgt_file) || bishop_reachable?(queen, tgt_rank, tgt_file)
+      end
+
+      # Collects pieces on the path of a specific queen moving to the target square, excluding the two ends.
+      # If the target square is not queen-reachable, throws a StandardError.
+      def collect_path(queen, tgt_rank, tgt_file)
+        board = queen.board
+        pos = pos_of(queen)
+        direction_vec = direction_of(queen, tgt_rank, tgt_file)
+        direction_unit = direction_vec.unitize
+        path = []
+
+        (1...direction_vec.len).each do |i|
+          dest = pos + direction_unit * i
+          path << board.piece_at(dest.rank, dest.file)
+        end
+        path
+      end
+
+      # Returns true if there's any piece on the path of a queen moving to the target square.
+      # Also works well with rooks and bishops.
+      def queen_blocked?(queen, tgt_rank, tgt_file)
+        collect_path(queen, tgt_rank, tgt_file).any? { |piece| !piece.nil? }
+      end
+    end
+
+    module KnightHelpers
+    end
+
+    module KingHelpers
+    end
+
+    module PawnHelpers
+    end
+  end
+end
